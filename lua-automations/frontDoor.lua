ab6e84d44480cfd69dedb8d06ecf1e8241da908c
Dave Della Costa <ddellacosta@gmail.com>
2023-03-19 01:08:18 +0000

    added blink in living room light strip to frontDoor opening script

--- lua-automations/frontDoor.lua
+++ lua-automations/frontDoor.lua
@@ -2,10 +2,12 @@
 local frontDoorSensor = nil
 local frontDoorSensorChan = nil
 local basementMirrorLight = nil
+local livingRoomWindowLight = nil
 
 function setup ()
    frontDoorSensor = register("0x0015bc001e00f658")
    basementMirrorLight = register("0xb4e3f9fffe14c707")
+   livingRoomWindowLight = register("0x680ae2fffe4577ac")
    frontDoorSensorChan = subscribe(frontDoorSensor.topic)
 end
 
@@ -17,6 +19,7 @@ function loop ()
       -- would like to have some helper that lets me run this and then
       -- reset prior state...maybe something like `publishAndRestore`?
       publish(basementMirrorLight.topicSet, { effect = "blink" })
+      publish(livingRoomWindowLight.topicSet, { effect = "blink" })
    end
 
    sleep(1)
