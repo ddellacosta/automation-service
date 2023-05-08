ba029b98ba97d90646ceb95eb6fac1da1ac3ab3b
Dave Della Costa <ddellacosta@gmail.com>
2023-05-08 17:36:27 -0400

    calculates sunrise and sunset locally

--- src/Service/DateHelpers.hs
+++ src/Service/DateHelpers.hs
@@ -1,7 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+
 module Service.DateHelpers
   ( addMinutes
   , dayFromHour
   , getCurrentDateString
+  , getCurrentZonedTime
+  , getSunriseAndSunset
+  , getTimeZone
   , minuteDiff
   , todayFromHour
   , utcTimeToCronInstant
@@ -10,12 +15,21 @@ where
 
 import Prelude hiding (min)
 
+import Control.Exception (IOException, handle)
+import Control.Lens ((^?), _1, _Just)
+import qualified Data.Astro.Sun as AstroSun
+import qualified Data.Astro.Time.Conv as AstroConv
+import qualified Data.Astro.Types as AstroTypes
 import Data.Fixed (Pico)
 import qualified Data.Time.Clock as C
 import Data.Time.Clock (NominalDiffTime, UTCTime)
 import qualified Data.Time.Format as F
 import Data.Time.Format (FormatTime)
 import qualified Data.Time.Format.ISO8601 as ISO
+import qualified Data.Time.LocalTime as LT
+import Data.Time.LocalTime (TimeZone, ZonedTime)
+import qualified Data.Time.Zones as Z
+import System.Environment (getEnv)
 
 -- | Given Pico seconds returns a NominalDiffTime for that number of
 -- seconds.
@@ -48,7 +62,7 @@ todayFromHour hourString =
 --
 getCurrentDateString :: IO String
 getCurrentDateString =
-  formatForDateString <$> C.getCurrentTime
+  formatForDateString <$> getCurrentZonedTime
 
 -- | Returns any timestamp with a FormatTime instance in the format
 -- "2023-05-05"
@@ -65,3 +79,51 @@ dayFromHour hourString date =
   ISO.iso8601ParseM (dateString <> "T" <> hourString <> ":00Z")
   where
     dateString = formatForDateString date
+
+getCurrentZonedTime :: IO ZonedTime
+getCurrentZonedTime = C.getCurrentTime >>= getZonedTimeForUTC
+
+getZonedTimeForUTC :: UTCTime -> IO ZonedTime
+getZonedTimeForUTC ts = do
+  tz <- getTimeZone ts
+  pure $ LT.utcToZonedTime tz ts
+
+getTimeZone :: UTCTime -> IO TimeZone
+getTimeZone ts = do
+  tzVar <- handle (\(_e :: IOException) -> pure "UTC") $ getEnv "TZ"
+  tz <- Z.loadTZFromDB tzVar
+  pure $ Z.timeZoneForUTCTime tz ts
+
+getSunriseAndSunset :: UTCTime -> (Double, Double) -> IO (Maybe UTCTime, Maybe UTCTime)
+getSunriseAndSunset ts (lat, long) = do
+  utcTs <- getZonedTimeForUTC ts
+  let
+    sunEvents@(sunrise, sunset) = getSunriseAndSunset' utcTs (lat, long)
+
+  if sunset < sunrise
+    then do
+      sunset' <- getNextDaySunset ts (lat, long)
+      pure (sunrise, sunset')
+    else
+      pure sunEvents
+
+  where
+    getNextDaySunset :: UTCTime -> (Double, Double) -> IO (Maybe UTCTime)
+    getNextDaySunset ts' (lat', long') = do
+      nextDay <- getZonedTimeForUTC . addMinutes 1440 $ ts'
+      let
+        (_sunrise, sunset) = getSunriseAndSunset' nextDay (lat', long')
+      pure sunset
+
+getSunriseAndSunset' :: ZonedTime -> (Double, Double) -> (Maybe UTCTime, Maybe UTCTime)
+getSunriseAndSunset' ts (lat, long) = (sunrise', sunset')
+  where
+    coords =
+      AstroTypes.GeoC (AstroTypes.DD lat) (AstroTypes.DD long)
+    -- see docs for sunRiseAndSet:
+    verticalShift = (AstroTypes.DD 0.833333)
+    lcd = AstroConv.zonedTimeToLCD ts
+    -- fno-warn-incomplete-uni-patterns at the top is for this
+    (AstroSun.RiseSet sunrise sunset) = AstroSun.sunRiseAndSet coords verticalShift lcd
+    sunrise' = LT.zonedTimeToUTC . AstroConv.lctToZonedTime <$> sunrise ^? _Just . _1
+    sunset' = LT.zonedTimeToUTC . AstroConv.lctToZonedTime <$> sunset ^? _Just . _1
--- test/Test/Unit/Service/DateHelpers.hs
+++ test/Test/Unit/Service/DateHelpers.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
+
 module Test.Unit.Service.DateHelpers
   ( spec
   ,
@@ -7,6 +9,7 @@ where
 import Control.Lens (filtered, preview)
 import Data.Aeson (Value(String))
 import Data.Aeson.Lens (_String, key, values)
+import Data.Foldable (for_)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import Data.Text (Text)
@@ -18,9 +21,11 @@ import Test.Hspec (Spec, describe, it, shouldBe)
 import Service.DateHelpers
   ( addMinutes
   , dayFromHour
+  , getSunriseAndSunset
   , utcTimeToCronInstant
   )
 import qualified System.Cron.Parser as P
+import System.Environment (setEnv)
 
 spec :: Spec
 spec = describe "date utility functions" $ do
@@ -59,6 +64,48 @@ spec = describe "date utility functions" $ do
     show (P.parseCronSchedule . T.pack . utcTimeToCronInstant $ thirtyBeforeSunset)
       `shouldBe`
       "Right CronSchedule 24 23 1 5 1"
+
+  it "correctly generates sunrise and sunset values according to TZ env var" $ do
+    let
+      locations =
+        [ ("America/New_York", (41.5020948, (-73.982543)))
+        , ("Europe/Kyiv", (50.45, 30.523333))
+        , ("Asia/Tokyo", (35.689722, 139.692222))
+        , ("Africa/Cairo", (30.044444, 31.235833))
+        , ("America/Mendoza", ((-32.883333), (-68.816667)))
+        , ("Pacific/Honolulu", (21.308889, (-157.826111)))
+        , ("Pacific/Auckland", ((-36.840556), 174.74))
+        ]
+
+    for_ locations $ \(tzStr, coords) -> do
+      setEnv "TZ" tzStr
+      utcNow <- C.getCurrentTime
+
+      let
+        testDate = fromMaybe utcNow $
+          ISO.iso8601ParseM "2023-05-08T20:00:00+00:00"
+      sunEvents@(sunrise, sunset) <- getSunriseAndSunset testDate coords
+
+      -- sunset should not be before sunrise
+      sunset < sunrise `shouldBe` False
+
+      (show sunEvents)
+        `shouldBe`
+        case tzStr of
+          "America/New_York" ->
+            "(Just 2023-05-08 09:44:34.938793480396 UTC,Just 2023-05-09 00:01:04.509460330009 UTC)"
+          "Europe/Kyiv" ->
+            "(Just 2023-05-09 02:20:05.546441674232 UTC,Just 2023-05-09 17:29:46.558659374713 UTC)"
+          "Asia/Tokyo" ->
+            "(Just 2023-05-09 19:41:06.513182222843 UTC,Just 2023-05-10 09:34:51.927377879619 UTC)"
+          "Africa/Cairo" ->
+            "(Just 2023-05-09 03:06:03.68356347084 UTC,Just 2023-05-09 16:37:30.942675769329 UTC)"
+          "America/Mendoza" ->
+            "(Just 2023-05-08 11:13:29.21167731285 UTC,Just 2023-05-08 21:49:50.850725769996 UTC)"
+          "Pacific/Honolulu" ->
+            "(Just 2023-05-08 15:56:27.287977337837 UTC,Just 2023-05-09 04:59:31.597565710544 UTC)"
+          "Pacific/Auckland" ->
+            "(Just 2023-05-09 19:07:33.402958810329 UTC,Just 2023-05-10 05:27:09.667979478836 UTC)"
 
   where
     mkUtcTimeFromVal :: Text -> UTCTime -> Value -> IO (Maybe UTCTime)
