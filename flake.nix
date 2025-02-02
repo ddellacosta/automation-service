bd3ddebc2d17a8fe3bc8e19c7c9b3f3dd76f00ef
Dave Della Costa <ddellacosta@gmail.com>
2025-02-02 00:19:04 -0500

    disable flaky test; downgrade some dependencies to get consistent builds between cabal and nix build; temporarily enable builds on this branch to test this

--- flake.nix
+++ flake.nix
@@ -66,14 +66,21 @@
         # - imageDigest = "sha256:31b808456afccc2a419507ea112e152cf27e9bd2527517b0b6ca8639cc423501";
         # - sha256 = "0bbw3r0civlcm3inj23fq8f25aw63rnaay09qjbrvfjd7pcfbyqn";
         # - finalImageTag = "2.15.0";
+
         #
-        nixFromDockerHub = pkgs.dockerTools.pullImage {
-          imageName = "nixos/nix";
-          imageDigest = "sha256:fd7a5c67d396fe6bddeb9c10779d97541ab3a1b2a9d744df3754a99add4046f1";
-          sha256 = "1izbcfac0cac0jg1q3x834lkdqc0q2dh670bj0zsyadhn0m7f3v6";
-          finalImageName = "nixos/nix";
-          finalImageTag = "2.24.9";
-        };
+        # previous ^ when bumped on 2025-02-01
+        #
+        #  imageDigest = "sha256:fd7a5c67d396fe6bddeb9c10779d97541ab3a1b2a9d744df3754a99add4046f1";
+        #  sha256 = "1izbcfac0cac0jg1q3x834lkdqc0q2dh670bj0zsyadhn0m7f3v6";
+        #  finalImageTag = "2.24.9";
+        #
+        # nixFromDockerHub = pkgs.dockerTools.pullImage {
+        #   imageName = "nixos/nix";
+        #   imageDigest = "sha256:a91fd903ad94ddde7d41257e0291973771eca41b56473f969e5860814dd5fd2c";
+        #   sha256 = "1ni4dwqxr8540gsnn1ch5vdj2f2v4yy35z4dss72vlx8x3j78ac4";
+        #   finalImageName = "nixos/nix";
+        #   finalImageTag = "2.26.1";
+        # };
 
       in {
         packages = {
@@ -82,7 +89,7 @@
           default = pkgs.dockerTools.buildImage {
             name = "automation-service";
             created = "now";
-            fromImage = nixFromDockerHub;
+            # fromImage = nixFromDockerHub;
 
             extraCommands = ''
               mkdir ./app
