ddellacosta/automation-service

commit 4f9c8f6d03d76e35672f681b5d7b448530b5a3b7

Generates a new index page that collects all branch test runs together and displays them on a single page (#40)

.github/workflows/main.yml
@@ -140,6 +140,15 @@ jobs:
       - name: Checkout
         uses: actions/checkout@v4
 
+      - uses: cachix/install-nix-action@v25
+        with:
+          github_access_token: ${{ secrets.GITHUB_TOKEN }}
+
+      - uses: cachix/cachix-action@v14
+        with:
+          name: ddellacosta
+          authToken: '${{ secrets.CACHIX_AUTH_TOKEN }}'
+
       - name: Download frontend results
         uses: actions/download-artifact@v4
         with:
@@ -206,6 +215,13 @@ jobs:
           report_id: 'backend-tests'
           gh_pages: 'ghp'
           report_dir: 'allure-results-backend'
+
+      - name: Update Allure Index Page to Include New Branch
+        shell: bash
+        run: |
+          set -euo pipefail
+          nix build .#allure-site-generator
+          result/bin/generate-allure-site
 
       - name: Git Commit and Push Action
         uses: mgrybyk-org/git-commit-pull-push-action@v1

flake.nix
@@ -26,7 +26,23 @@
         };
         t = pkgs.lib.trivial;
         hl = pkgs.haskell.lib;
-        haskellPackages = pkgs.haskell.packages.ghc964;
+        haskellPackagesWithoutOverrides = pkgs.haskell.packages.ghc964;
+
+        haskellPackages = haskellPackagesWithoutOverrides.override {
+          overrides = self: super: {
+            astro = hl.dontCheck (hl.markUnbroken super.astro);
+
+            # https://github.com/stoeffel/tasty-test-reporter/pull/13
+            tasty-test-reporter = self.callCabal2nix "tasty-test-reporter"
+              (pkgs.fetchFromGitHub {
+                owner = "jberthold";
+                repo = "tasty-test-reporter";
+                rev = "3abbfa942553f0986393af44eda0cb69bcaba6ad";
+                # ref = "bump-upper-bounds-and-enable-tasty-1.5";
+                sha256 = "sha256-lyOmsC4nm5ofmRrUawkXFERzyU0WERlZ3x+g/7l7sCM=";
+              }) {};
+          };
+        };
 
         name = "automation-service";
 
@@ -120,23 +136,9 @@
             inherit automation-service-ui-npm-deps node_version pkgs;
           };
 
-        haskellOverrides = self: super: {
-          astro = hl.dontCheck (hl.markUnbroken super.astro);
-
-          # https://github.com/stoeffel/tasty-test-reporter/pull/13
-          tasty-test-reporter = self.callCabal2nix "tasty-test-reporter"
-            (pkgs.fetchFromGitHub {
-              owner = "jberthold";
-              repo = "tasty-test-reporter";
-              rev = "3abbfa942553f0986393af44eda0cb69bcaba6ad";
-              # ref = "bump-upper-bounds-and-enable-tasty-1.5";
-              sha256 = "sha256-lyOmsC4nm5ofmRrUawkXFERzyU0WERlZ3x+g/7l7sCM=";
-            }) {};
-        };
-
-        automation-service = devTools:
+        automation-service =
           let
-            addBuildTools = (t.flip hl.addBuildTools) (devTools ++ [
+            addBuildTools = (t.flip hl.addBuildTools) ( [
               automation-service-ui
               pkgs.zlib
             ]);
@@ -144,14 +146,11 @@
             haskellPackages.developPackage {
               root = ./.;
               name = name;
-              returnShellEnv = !(devTools == []);
-
-              overrides = haskellOverrides;
 
               modifier = (t.flip t.pipe) [
                 addBuildTools
                 hl.dontHaddock
-                hl.dontCheck # Don't run tests during build
+                hl.dontCheck # Don't run tests during real build
                 hl.enableExecutableProfiling
                 (drv: hl.overrideCabal drv (attrs: {
                   configureFlags = [
@@ -161,11 +160,50 @@
               ];
             };
 
+        #
+        # I need this separate for a dev environment because
+        # apparently hl.dontCheck prevents my test dependency
+        # overrides (tasty-test-reporter) from being visible to cabal:
+        #
+        automation-service-dev =
+          haskellPackages.developPackage {
+            root = ./.;
+            name = name;
+            returnShellEnv = true;
+
+            modifier = (t.flip t.pipe) [
+              hl.dontHaddock
+              hl.enableExecutableProfiling
+              (drv: hl.overrideCabal drv (attrs: {
+                configureFlags = [
+                  "--ghc-options=-fprof-auto -fno-prof-count-entries"
+                ];
+              }))
+            ];
+          };
+
         automation-service-test =
           import ./nix/backend-test.nix {
-            inherit automation-service-npm-deps node_version pkgs; 
-            overrides = haskellOverrides;
+            inherit automation-service-npm-deps automation-service-ui haskellPackages node_version pkgs;
           };
+
+        allure-site-generator = devTools:
+          let
+            addBuildTools = (t.flip hl.addBuildTools) (devTools ++ [
+              pkgs.zlib
+            ]);
+          in
+            haskellPackages.developPackage {
+              root = ./generate-allure-site/.;
+              name = "generate-allure-site";
+              returnShellEnv = !(devTools == []);
+
+              modifier = (t.flip t.pipe) [
+                addBuildTools
+                hl.dontHaddock
+                hl.enableExecutableProfiling
+              ];
+            };
 
       in {
         packages = {
@@ -173,8 +211,10 @@
           automation-service-ui = automation-service-ui;
           automation-service-ui-test = automation-service-ui-test;
 
-          automation-service = automation-service [ ];
+          automation-service = automation-service;
           automation-service-test = automation-service-test;
+
+          allure-site-generator = allure-site-generator [];
 
           default = pkgs.dockerTools.buildImage {
             name = "automation-service";
@@ -213,28 +253,32 @@
           };
         };
 
-        devShell = automation-service (with haskellPackages; [
-          cabal-fmt
-          cabal-install
-          ghc-events
-          ghcid
-          haskell-language-server
-          hlint
-          node_version
-          pkgs.allure
-          pkgs.chromium
-          pkgs.esbuild
-          pkgs.jq
-          pkgs.libxml2 # for xmllint
-          pkgs.lua
-          pkgs.mosquitto
-          pkgs.prefetch-npm-deps
-          pkgs.purs
-          pkgs.skopeo # for calculating sha256 of docker image
-          pkgs.spago-unstable
-          pkgs.watchexec
-          stylish-haskell
-          # threadscope # marked as broken :-(
-        ]);
+        devShell = pkgs.mkShell {
+          inputsFrom = [ automation-service-dev ];
+          nativeBuildInputs = (with haskellPackages; [
+            cabal-fmt
+            cabal-install
+            ghc-events
+            ghcid
+            haskell-language-server
+            hlint
+            node_version
+            pkgs.allure
+            pkgs.chromium
+            pkgs.esbuild
+            pkgs.jq
+            pkgs.libxml2 # for xmllint
+            pkgs.lua
+            pkgs.mosquitto
+            pkgs.prefetch-npm-deps
+            pkgs.purs
+            pkgs.skopeo # for calculating sha256 of docker image
+            pkgs.spago-unstable
+            pkgs.watchexec
+            pkgs.zlib
+            stylish-haskell
+            # threadscope # marked as broken :-(
+          ]);
+        };
       });
 }

generate-allure-site/app/Main.hs
@@ -1,0 +1,263 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Lens ((^?), to)
+import Data.Aeson.Lens (_Array, _Integer, _String, key, nth)
+import Data.ByteString.Lazy (ByteString, writeFile, readFile)
+import Data.Foldable (fold, foldl', foldMap)
+import Data.List (maximum, sortBy, zipWith)
+import Data.Maybe (Maybe, fromMaybe)
+import Data.String (String, fromString)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Traversable (for)
+import qualified Data.Vector as V
+import Network.Wai.Application.Static (defaultFileServerSettings, staticApp)
+import Network.Wai.Handler.Warp (run)
+import Prelude (IO, Int, Integer, Show, (.), ($), (<$>), (<>), (-), (==), compare, flip, fst, max, mempty, pure, putStrLn, show, snd)
+import System.Directory (listDirectory, setCurrentDirectory)
+import System.Environment (getArgs, getProgName)
+import Text.Blaze ((!))
+import Text.Blaze.Html (Html, toHtml)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Internal (customAttribute)
+import Text.Blaze.Renderer.Utf8 (renderMarkup)
+import Text.Read (readMaybe)
+
+
+allurePath :: String -> String -> String
+allurePath branch suite = "allure-action/" <> branch <> "/" <> suite
+
+integrity :: H.AttributeValue -> H.Attribute
+integrity = customAttribute "integrity"
+
+crossorigin :: H.AttributeValue -> H.Attribute
+crossorigin = customAttribute "crossorigin"
+
+dataBsToggle :: H.AttributeValue -> H.Attribute
+dataBsToggle = customAttribute "data-bs-toggle"
+
+dataBsTarget :: H.AttributeValue -> H.Attribute
+dataBsTarget = customAttribute "data-bs-target"
+
+dataBsParent :: H.AttributeValue -> H.Attribute
+dataBsParent = customAttribute "data-bs-parent"
+
+-- strings in Haskell, always fun
+toText :: Show a => a -> Text
+toText = T.pack . show
+
+-- doing it in this convoluted way so that I can rely upon
+-- JS providing l10n/i18n format for the client
+localizedTimestamp :: String -> String -> Text -> Int -> Html
+localizedTimestamp branch suite ts idx' = H.td $ do
+  let
+    tsId =
+      T.pack branch <> "-" <> T.pack suite <> "-ts-" <> ts <> "-" <> (T.pack . show $ idx')
+  H.div
+    ! (A.id . fromString . T.unpack $ tsId)
+    $ H.script $ toHtml $
+        "document.getElementById('"
+      <> tsId
+      <> "').textContent = new Date("
+      <> ts
+      <> ").toLocaleString();"
+
+--
+-- Kinda bad how this will fail if one of these values is a Nothing,
+-- but this is hacky utility code for generating test result views, so
+-- if nothing shows up for a bunch of rows that's my signal this broke
+-- and I need to check the data or make this more robust. For now it's
+-- not worth the effort.
+--
+branchRunRowHtml :: String -> String -> Int -> ByteString -> Maybe (Integer, [Html])
+branchRunRowHtml branch suite idx fileData = do
+  runUniqueId <- fileData ^? nth idx . key "runUniqueId" . _String
+
+  let
+    newTimestampInteger = fromMaybe 0 $ fileData ^? nth idx . key "timestamp" . _Integer
+    newTimestamp = toText newTimestampInteger
+
+  passed <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "passed" . _Integer
+  failed <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "failed" . _Integer
+  broken <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "broken" . _Integer
+  skipped <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "skipped" . _Integer
+  total <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "total" . _Integer
+  duration <- toText <$> fileData ^? nth idx . key "summary" . key "time" . key "duration" . _Integer
+  testResult <- fileData ^? nth idx . key "testResult" . _String
+
+  pure $
+    ( newTimestampInteger
+    , [H.tr
+          ! A.class_ (passOrFailClass testResult)
+          $ fold $
+               [localizedTimestamp branch suite newTimestamp idx]
+            <> (H.td . toHtml <$> [passed, failed, broken, skipped, total, duration])
+            <> [H.td $ H.a ! A.href (testRunLink . T.unpack $ runUniqueId) $ toHtml runUniqueId]
+      ]
+    )
+
+  where
+    passOrFailClass = \case
+      "PASS" -> "table-success"
+      "FAIL" -> "table-danger"
+      _ -> "table-secondary" -- ¯\_(ツ)_/¯
+
+    testRunLink :: String -> H.AttributeValue
+    testRunLink runUniqueId = fromString $
+      allurePath branch suite <> "/" <> runUniqueId <> "/"
+
+--
+-- Does the majority of the heavy lifting with stuffing branch rows
+-- into Html (tr) and extracting the most recent timestamp from each
+-- branch so we can compare later for sorting.
+--
+branchesRunsHtml :: [String] -> IO [(Integer, String, [(String, [Html])])]
+branchesRunsHtml branches =
+  for branches $ \branch -> do
+
+    -- collects the most recent timestamp and tuples of
+    -- suite/collected Html rows for that branch suite
+    branchRuns :: [(Integer, (String, [Html]))] <-
+      for ["frontend-tests", "backend-tests"] $ \suite -> do
+        fileData <- readFile $ allurePath branch suite <> "/data.json"
+
+        let
+          -- lens is kind of insane
+          length = fromMaybe 0 $ fileData ^? _Array . to V.length
+
+          -- already exists somewhere?
+          ffoldl' :: b -> [a] -> (b -> a -> b) -> b
+          ffoldl' b ta f = foldl' f b ta
+
+          -- returns the most recent timestamp for the set of runs in
+          -- this branch/suite combo, along with the collected Html
+          -- generated from each row
+          (maxTimestamp', branchSuiteRuns) :: (Integer, [Html]) =
+            ffoldl' (0, []) [0..(length - 1)] $ \(maxTimestamp, branchSuiteHtml) idx ->
+              let
+                (newTimestamp, branchSuiteRowHtml) =
+                  fromMaybe (0, []) $
+                    branchRunRowHtml branch suite idx fileData
+              in
+                (max newTimestamp maxTimestamp, branchSuiteHtml <> branchSuiteRowHtml)
+
+        pure (maxTimestamp', (suite, branchSuiteRuns))
+
+    -- return the most recent timestamp in the branch out of both
+    -- suites, the branch name, and a list of each suite and its
+    -- collected Html rows for all runs
+    pure (maximum $ fst <$> branchRuns, branch, snd <$> branchRuns)
+
+--
+-- Takes the collected HTML for each given branch and displays it
+-- inside Bootstrap accordions
+-- (https://getbootstrap.com/docs/5.3/components/accordion/). The top
+-- position (most recent branch built by CI) only will be uncollapsed
+-- by default.
+--
+branchesHtml :: Int -> String -> [(String, [Html])] -> Html
+branchesHtml pos branch branchRunsHtml = do
+  let parentId = "branch-" <> branch
+  H.h2 ! A.class_ "m-2 mt-4 p-1" $ toHtml branch
+  H.div
+    ! A.class_ "accordion"
+    ! (A.id . fromString $ parentId) $
+    H.div ! A.class_ "accordion-item" $
+      (flip foldMap) branchRunsHtml $ \(suite, branchSuiteRuns) -> do
+        let targetId = "collapse-" <> branch <> "-" <> suite
+        H.h3 ! A.class_ "accordion-header" $
+          H.button
+          -- if we're at the top of the list (most recent) we show
+          -- this opened up by default; otherwise we collapse
+            ! (A.class_ $
+                "accordion-button" <> (if pos == 0 then "" else " collapsed"))
+            ! dataBsToggle "collapse"
+            ! (dataBsTarget . fromString $ "#" <> targetId)
+            ! A.type_ "button"
+            $ toHtml suite
+        H.div
+        -- see above comment re: first position
+          ! (A.class_ $
+              "accordion-collapse collapse" <> (if pos == 0 then " show" else ""))
+          ! (A.id . fromString $ targetId)
+          ! (dataBsParent . fromString $ "#" <> parentId) $
+          H.div ! A.class_ "accordion-body" $
+            H.div ! A.class_ "accordion-item" $ branchLink branchSuiteRuns
+
+  where
+    branchLink :: [Html] -> Html
+    branchLink testResults =
+      H.table ! A.class_ "table" $ do
+        H.thead $
+          H.tr $
+            foldMap (H.th . toHtml)
+              (["Date", "Pass", "Fail", "Broken", "Skip", "Total", "Duration", "Test Run"] :: [Text])
+        H.tbody $ fold testResults
+
+--
+-- Stuffs the collected branch HTML into the main page template
+--
+branchesPage :: Html -> Html
+branchesPage branches = do
+  H.html $ do
+    H.head $ do
+      H.title "automation-service - Branch Test Runs"
+      H.link
+        ! A.rel "stylesheet"
+        ! A.type_ "text/css"
+        ! A.href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        ! integrity "sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+        ! crossorigin "anonymous"
+
+      H.script
+        ! A.src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        ! integrity "sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        ! crossorigin "anonymous"
+        $ mempty
+
+    H.body $
+      H.div ! A.class_ "container" $ do
+        H.h2 ! A.class_ "m-2 p-1" $ "automation-service - Branch Test Runs"
+        branches
+
+--
+-- this is hacky test page generation code and I'm not being very
+-- careful about configuration, so keep in mind how many assumptions
+-- this makes about where things are--this is deeply coupled to what
+-- is set up in ./github/workflows/main.yml
+--
+generateSite :: IO ()
+generateSite = do
+  setCurrentDirectory "ghp"
+  files <- listDirectory "allure-action"
+  branchesRuns :: [(Integer, String, [(String, [Html])])] <- branchesRunsHtml files
+  let
+    branchesRuns' = sortBy (\(ts1, _, _) (ts2, _, _) -> compare ts2 ts1) branchesRuns
+    branchesRunsOutput =
+      -- seems like there's a more obvious function to map over a list
+      -- with an index that I'm missing but whatever
+      zipWith
+        (\idx (_ts, branch, branchRuns) -> branchesHtml idx branch branchRuns)
+        [0..]
+        branchesRuns'
+  writeFile "test-runs/index.html" (renderMarkup $ branchesPage $ fold branchesRunsOutput)
+
+serve :: IO ()
+serve = do
+  args <- getArgs
+  let
+    port = case args of
+      (p:_) -> fromMaybe 8000 (readMaybe p)
+      _     -> 8000
+  putStrLn $ "Starting static file server on port " <> show port <> "..."
+  run port $ staticApp (defaultFileServerSettings ".")
+
+main :: IO ()
+main = do
+  progName <- getProgName
+  case progName of
+    "serve" -> serve
+    _       -> generateSite

cabal.project
@@ -1,0 +1,4 @@
+-- Root workspace definition: include both packages
+packages:
+  ./
+  generate-allure-site/

cabal.project.local
@@ -1,3 +1,0 @@
-constraints: lua +system-lua +pkg-config +apicheck
-ignore-project: False
-tests: True

generate-allure-site/generate-allure-site.cabal
@@ -1,0 +1,53 @@
+cabal-version:      3.4
+name:               generate-allure-site
+version:            0.1.0.0
+-- synopsis:
+-- description:
+license:            BSD-3-Clause
+license-file:       LICENSE
+author:             Dave Della Costa
+maintainer:         ddellacosta@gmail.com
+-- copyright:
+category:           Testing
+build-type:         Simple
+extra-doc-files:    CHANGELOG.md
+-- extra-source-files:
+
+common shared
+  ghc-options:      -Wall
+  default-language: GHC2021
+
+  build-depends:
+      aeson
+    , base ^>= 4.18.2.0
+    , blaze-html
+    , blaze-markup
+    , bytestring
+    , containers
+    , directory
+    , lens
+    , lens-aeson
+    , pretty-simple
+    , tagsoup 
+    , text
+    , vector
+    , wai-app-static
+    , warp
+
+  default-extensions:
+      DeriveGeneric
+    , FunctionalDependencies
+    , LambdaCase
+    , OverloadedStrings
+    , StrictData
+    , TupleSections
+
+executable generate-allure-site
+  import:           shared
+  main-is:          Main.hs
+  hs-source-dirs:   app
+
+executable serve
+  import:           shared
+  main-is:          Main.hs
+  hs-source-dirs:   app

nix/backend-test.nix
@@ -1,17 +1,13 @@
 { automation-service-npm-deps
+, automation-service-ui
 , node_version
-, overrides
+, haskellPackages
 , pkgs
 , src ? ../.
 }:
 
 let
   hl = pkgs.haskell.lib;
-
-  # Customize the Haskell package set if needed
-  haskellPackages = pkgs.haskell.packages.ghc964.override {
-    overrides = overrides;
-  };
 
   base = haskellPackages.callCabal2nix "automation-service" src { };
   baseNoDocs = hl.dontHaddock base;

commit 5ff960d714f98b4d5a1664da288c0a26679fcaf2

fix broken test run index page links (#41)
I didn't take into account the fact that I moved the test index page to a sub-directory (/test-runs/) in the links to individual branch runs. Whoops. Made this a configurable value so I can run it locally and in CI and have it work in both environments.

Also it turns out I didn't actually fix the broken overridden dependencies in #40 , so I remedied that for real--I think, I tested it reasonably well this time I feel like! But I thought so last time too apparently

.github/workflows/main.yml
@@ -216,12 +216,12 @@ jobs:
           gh_pages: 'ghp'
           report_dir: 'allure-results-backend'
 
-      - name: Update Allure Index Page to Include New Branch
+      - name: Update Allure Index Page to Include Branch Runs
         shell: bash
         run: |
           set -euo pipefail
           nix build .#allure-site-generator
-          result/bin/generate-allure-site
+          result/bin/generate-allure-site /automation-service/
 
       - name: Git Commit and Push Action
         uses: mgrybyk-org/git-commit-pull-push-action@v1

flake.nix
@@ -165,22 +165,31 @@
         # apparently hl.dontCheck prevents my test dependency
         # overrides (tasty-test-reporter) from being visible to cabal:
         #
-        automation-service-dev =
-          haskellPackages.developPackage {
-            root = ./.;
-            name = name;
-            returnShellEnv = true;
+        automation-service-dev = devTools:
+          let
+            addBuildTools = (t.flip hl.addBuildTools) (devTools ++ [
+              automation-service-ui
+              pkgs.zlib
+            ]);
+          in
+            haskellPackages.developPackage {
+              # this prevents CHANGELOG/LICENSE/etc. from being found
+              # root = lib.sourceFilesBySuffices ./. [ ".cabal" ".hs" ];
+              root = ./.;
+              name = name;
+              returnShellEnv = true;
 
-            modifier = (t.flip t.pipe) [
-              hl.dontHaddock
-              hl.enableExecutableProfiling
-              (drv: hl.overrideCabal drv (attrs: {
-                configureFlags = [
-                  "--ghc-options=-fprof-auto -fno-prof-count-entries"
-                ];
-              }))
-            ];
-          };
+              modifier = (t.flip t.pipe) [
+                addBuildTools
+                hl.dontHaddock
+                hl.enableExecutableProfiling
+                (drv: hl.overrideCabal drv (attrs: {
+                  configureFlags = [
+                    "--ghc-options=-fprof-auto -fno-prof-count-entries"
+                  ];
+                }))
+              ];
+            };
 
         automation-service-test =
           import ./nix/backend-test.nix {
@@ -253,32 +262,35 @@
           };
         };
 
-        devShell = pkgs.mkShell {
-          inputsFrom = [ automation-service-dev ];
-          nativeBuildInputs = (with haskellPackages; [
-            cabal-fmt
-            cabal-install
-            ghc-events
-            ghcid
-            haskell-language-server
-            hlint
-            node_version
-            pkgs.allure
-            pkgs.chromium
-            pkgs.esbuild
-            pkgs.jq
-            pkgs.libxml2 # for xmllint
-            pkgs.lua
-            pkgs.mosquitto
-            pkgs.prefetch-npm-deps
-            pkgs.purs
-            pkgs.skopeo # for calculating sha256 of docker image
-            pkgs.spago-unstable
-            pkgs.watchexec
-            pkgs.zlib
-            stylish-haskell
-            # threadscope # marked as broken :-(
-          ]);
-        };
+        # pkgs.mkShell using inputsFrom [ automation-service-dev] and
+        # setting build inputs using nativeBuildInputs seems to not
+        # include test development dependencies so going back to this
+        # way of setting up the devShell:
+        #
+        devShell = automation-service-dev (with haskellPackages; [
+          cabal-fmt
+          cabal-install
+          ghc-events
+          ghcid
+          haskell-language-server
+          hlint
+          node_version
+          pkgs.allure
+          pkgs.chromium
+          pkgs.esbuild
+          pkgs.jq
+          pkgs.libxml2 # for xmllint
+          pkgs.lua
+          pkgs.mosquitto
+          pkgs.prefetch-npm-deps
+          pkgs.purs
+          pkgs.skopeo # for calculating sha256 of docker image
+          pkgs.spago-unstable
+          pkgs.watchexec
+          pkgs.zlib
+          stylish-haskell
+          # threadscope # marked as broken :-(
+        ]);
+
       });
 }

generate-allure-site/app/Main.hs
@@ -72,8 +72,8 @@ localizedTimestamp branch suite ts idx' = H.td $ do
 -- and I need to check the data or make this more robust. For now it's
 -- not worth the effort.
 --
-branchRunRowHtml :: String -> String -> Int -> ByteString -> Maybe (Integer, [Html])
-branchRunRowHtml branch suite idx fileData = do
+branchRunRowHtml :: String -> String -> String -> Int -> ByteString -> Maybe (Integer, [Html])
+branchRunRowHtml rootPrefix branch suite idx fileData = do
   runUniqueId <- fileData ^? nth idx . key "runUniqueId" . _String
 
   let
@@ -107,15 +107,15 @@ branchRunRowHtml branch suite idx fileData = do
 
     testRunLink :: String -> H.AttributeValue
     testRunLink runUniqueId = fromString $
-      allurePath branch suite <> "/" <> runUniqueId <> "/"
+      rootPrefix <> allurePath branch suite <> "/" <> runUniqueId <> "/"
 
 --
 -- Does the majority of the heavy lifting with stuffing branch rows
 -- into Html (tr) and extracting the most recent timestamp from each
 -- branch so we can compare later for sorting.
 --
-branchesRunsHtml :: [String] -> IO [(Integer, String, [(String, [Html])])]
-branchesRunsHtml branches =
+branchesRunsHtml :: String -> [String] -> IO [(Integer, String, [(String, [Html])])]
+branchesRunsHtml rootPrefix branches =
   for branches $ \branch -> do
 
     -- collects the most recent timestamp and tuples of
@@ -140,7 +140,7 @@ branchesRunsHtml branches =
               let
                 (newTimestamp, branchSuiteRowHtml) =
                   fromMaybe (0, []) $
-                    branchRunRowHtml branch suite idx fileData
+                    branchRunRowHtml rootPrefix branch suite idx fileData
               in
                 (max newTimestamp maxTimestamp, branchSuiteHtml <> branchSuiteRowHtml)
 
@@ -223,6 +223,14 @@ branchesPage branches = do
         H.h2 ! A.class_ "m-2 p-1" $ "automation-service - Branch Test Runs"
         branches
 
+getRootPrefix :: IO String
+getRootPrefix = do
+  args <- getArgs
+  pure $ 
+    case args of
+      (rp:_) -> rp
+      _     -> ""
+
 --
 -- this is hacky test page generation code and I'm not being very
 -- careful about configuration, so keep in mind how many assumptions
@@ -231,9 +239,11 @@ branchesPage branches = do
 --
 generateSite :: IO ()
 generateSite = do
+  -- should probably make this configurable at some point
   setCurrentDirectory "ghp"
   files <- listDirectory "allure-action"
-  branchesRuns :: [(Integer, String, [(String, [Html])])] <- branchesRunsHtml files
+  rootPrefix <- getRootPrefix
+  branchesRuns :: [(Integer, String, [(String, [Html])])] <- branchesRunsHtml rootPrefix files
   let
     branchesRuns' = sortBy (\(ts1, _, _) (ts2, _, _) -> compare ts2 ts1) branchesRuns
     branchesRunsOutput =