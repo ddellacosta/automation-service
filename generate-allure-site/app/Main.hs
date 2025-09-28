2743596eb56ab5e3f84c634147c8371c72dcbc77
Dave Della Costa <ddellacosta@gmail.com>
2025-09-27 23:54:49 -0400

    Sorts branches by latest timestamp, displays more recently built branch at top un-collapsed

--- generate-allure-site/app/Main.hs
+++ generate-allure-site/app/Main.hs
@@ -4,8 +4,10 @@ module Main (main) where
 
 import Control.Lens ((^?), to)
 import Data.Aeson.Lens (_Array, _Integer, _String, key, nth)
-import Data.ByteString.Lazy (writeFile, readFile)
-import Data.Foldable (fold)
+import Data.ByteString.Lazy (ByteString, writeFile, readFile)
+import Data.Foldable (fold, foldl', foldMap)
+import Data.List (maximum, sortBy)
+import Data.Maybe (Maybe, fromMaybe)
 import Data.String (String, fromString)
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -13,7 +15,7 @@ import Data.Traversable (for)
 import qualified Data.Vector as V
 import Network.Wai.Application.Static (defaultFileServerSettings, staticApp)
 import Network.Wai.Handler.Warp (run)
-import Prelude (FilePath, IO, Int, (.), ($), (<$>), (<>), (-), flip, foldMap, mempty, pure, show)
+import Prelude (FilePath, IO, Int, Integer, Show, (.), ($), (<$>), (<>), (-), (==), compare, flip, fst, succ, max, mempty, pure, show, snd)
 import System.Directory (listDirectory, setCurrentDirectory)
 import System.Environment (getProgName)
 import Text.Blaze ((!))
@@ -23,6 +25,9 @@ import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Internal (customAttribute)
 import Text.Blaze.Renderer.Utf8 (renderMarkup)
 
+
+allurePath :: String -> String -> String
+allurePath branch suite = "allure-action/" <> branch <> "/" <> suite
 
 integrity :: H.AttributeValue -> H.Attribute
 integrity = customAttribute "integrity"
@@ -39,107 +44,132 @@ dataBsTarget = customAttribute "data-bs-target"
 dataBsParent :: H.AttributeValue -> H.Attribute
 dataBsParent = customAttribute "data-bs-parent"
 
+toText :: Show a => a -> Text
+toText = T.pack . show
 
-branchesHtml :: [FilePath] -> IO [Html]
-branchesHtml branches =
+-- doing it in this convoluted way so that I can rely upon
+-- JS providing l10n/i18n format for the client
+localizedTimestamp :: String -> String -> Text -> Int -> Html
+localizedTimestamp branch suite ts idx' = H.td $ do
+  let
+    tsId =
+      T.pack branch <> "-" <> T.pack suite <> "-ts-" <> ts <> "-" <> (T.pack . show $ idx')
+  H.div
+    ! (A.id . fromString . T.unpack $ tsId)
+    $ H.script $ toHtml $
+        "document.getElementById('"
+      <> tsId
+      <> "').textContent = new Date("
+      <> ts
+      <> ").toLocaleString();"
+
+ffoldl' :: b -> [a] -> (b -> a -> b) -> b
+ffoldl' b ta f = foldl' f b ta
+
+branchRunRowHtml :: String -> String -> Int -> ByteString -> Maybe (Integer, [Html])
+branchRunRowHtml branch suite idx fileData = do
+  --
+  -- I love how this could all just break immediately with a single Nothing lol
+  -- I'm just hackin' on some code brah
+  --
+  -- Okay but for real Data.Aeson.Lens is sick
+  --
+  runUniqueId <- fileData ^? nth idx . key "runUniqueId" . _String
+
+  let
+    newTimestampInteger = fromMaybe 0 $ fileData ^? nth idx . key "timestamp" . _Integer
+    newTimestamp = toText newTimestampInteger
+
+  passed <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "passed" . _Integer
+  failed <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "failed" . _Integer
+  broken <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "broken" . _Integer
+  skipped <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "skipped" . _Integer
+  total <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "total" . _Integer
+  duration <- toText <$> fileData ^? nth idx . key "summary" . key "time" . key "duration" . _Integer
+  testResult <- fileData ^? nth idx . key "testResult" . _String
+
+  pure $
+    ( newTimestampInteger
+    , [H.tr
+          ! A.class_ (passOrFailClass testResult)
+          $ fold $
+               [localizedTimestamp branch suite newTimestamp idx]
+            <> (H.td . toHtml <$> [passed, failed, broken, skipped, total, duration])
+            <> [H.td $ H.a ! A.href (testRunLink . T.unpack $ runUniqueId) $ toHtml runUniqueId]
+      ]
+    )
+
+  where
+    passOrFailClass = \case
+      "PASS" -> "table-success"
+      "FAIL" -> "table-danger"
+      _ -> "table-secondary" -- ¯\_(ツ)_/¯
+
+    testRunLink :: String -> H.AttributeValue
+    testRunLink runUniqueId = fromString $
+      allurePath branch suite <> "/" <> runUniqueId <> "/"
+
+branchesRunsHtml :: [FilePath] -> IO [(Integer, FilePath, [(FilePath, [Html])])]
+branchesRunsHtml branches =
   for branches $ \branch -> do
-    let
-      suites = ["frontend-tests", "backend-tests"]
-      allurePath suite = branch <> "/" <> suite
-      branchLink testResults =
-        H.table ! A.class_ "table" $ do
-          H.thead $
-            H.tr $
-              foldMap (H.th . toHtml)
-                (["Date", "Pass", "Fail", "Broken", "Skip", "Total", "Duration", "Test Run Link"] :: [Text])
-          H.tbody $ fold testResults
+    branchRuns :: [(Integer, (FilePath, [Html]))] <-
+      for ["frontend-tests", "backend-tests"] $ \suite -> do
+        fileData <- readFile $ allurePath branch suite <> "/data.json"
 
-    --
-    -- ✨ dynamic types ✨
-    --
-    branchRunsHtml <- for suites $ \suite -> do
-      fileData <- readFile $ "allure-action/" <> allurePath suite <> "/data.json"
-      branchRuns <- pure $ do
-        -- "Maybe this will work"-level code
-        length <- fileData ^? _Array . to V.length
+        let
+          mBranchSuiteRuns :: Maybe (Integer, [Html]) = do
+            -- "Maybe this will work"-level code
+            length <- fileData ^? _Array . to V.length
 
-        for [1..length] $ \n -> do
-          let
-            toText = T.pack . show
+            pure $
+              ffoldl' (0, []) [0..(length - 1)] $ \(maxTimestamp, branchSuiteHtml) idx ->
+                let
+                  (newTimestamp, branchSuiteRowHtml) =
+                    fromMaybe (0, []) $
+                      branchRunRowHtml branch suite idx fileData
+                in
+                  (max newTimestamp maxTimestamp, branchSuiteHtml <> branchSuiteRowHtml)
 
-            passOrFailClass = \case
-              "PASS" -> "table-success"
-              "FAIL" -> "table-danger"
-              _ -> "table-secondary" -- ¯\_(ツ)_/¯
+          (maxTimestamp', branchSuiteRuns) = fromMaybe (0, []) mBranchSuiteRuns
 
-            testRunLink :: String -> H.AttributeValue
-            testRunLink runUniqueId = fromString $
-              "allure-action/" <> allurePath suite <> "/" <> runUniqueId <> "/"
+        pure (maxTimestamp', (suite, branchSuiteRuns))
 
-            -- doing it in this convoluted way so that I can rely upon
-            -- JS providing l10n/i18n format for the client
-            localizedTimestamp :: Text -> Int -> Html
-            localizedTimestamp ts idx' = H.td $ do
-              let tsId = T.pack branch <> "-" <> T.pack suite <> "-ts-" <> ts <> "-" <> (T.pack . show $ idx')
-              H.div
-                ! (A.id . fromString . T.unpack $ tsId)
-                $ H.script $ toHtml $
-                    "document.getElementById('"
-                  <> tsId
-                  <> "').textContent = new Date("
-                  <> ts
-                  <> ").toLocaleString();"
+    pure (maximum $ fst <$> branchRuns, branch, snd <$> branchRuns)
 
-            idx = n - 1
+branchLink :: [Html] -> Html
+branchLink testResults =
+  H.table ! A.class_ "table" $ do
+    H.thead $
+      H.tr $
+        foldMap (H.th . toHtml)
+          (["Date", "Pass", "Fail", "Broken", "Skip", "Total", "Duration", "Test Run"] :: [Text])
+    H.tbody $ fold testResults
 
-          --
-          -- I love how this could all just break immediately with a single Nothing lol
-          -- I'm just hackin' on some code brah
-          --
-          -- Okay but for real Data.Aeson.Lens is sick
-          --
-          runUniqueId <- fileData ^? nth idx . key "runUniqueId" . _String
-          timestamp <- toText <$> fileData ^? nth idx . key "timestamp" . _Integer
-          passed <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "passed" . _Integer
-          failed <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "failed" . _Integer
-          broken <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "broken" . _Integer
-          skipped <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "skipped" . _Integer
-          total <- toText <$> fileData ^? nth idx . key "summary" . key "statistic" . key "total" . _Integer
-          duration <- toText <$> fileData ^? nth idx . key "summary" . key "time" . key "duration" . _Integer
-          testResult <- fileData ^? nth idx . key "testResult" . _String
-
-          pure $
-            H.tr ! A.class_ (passOrFailClass testResult) $ fold $
-              [localizedTimestamp timestamp idx]
-              <>
-              (H.td . toHtml <$> [passed, failed, broken, skipped, total, duration])
-              <>
-              [H.td $ H.a ! A.href (testRunLink . T.unpack $ runUniqueId) $ toHtml runUniqueId]
-
-      pure (suite, branchRuns)
-
-    pure $ do
-      let parentId = "branch-" <> branch
-      H.h2 ! A.class_ "m-2 mt-4 p-1" $ toHtml branch
-      H.div
-        ! A.class_ "accordion"
-        ! (A.id . fromString $ parentId)
-        $ H.div ! A.class_ "accordion-item" $
-          (flip foldMap) branchRunsHtml $ \(suite, branchRuns) -> do
-            let targetId = "collapse-" <> branch <> "-" <> suite
-            H.h3 ! A.class_ "accordion-header" $
-              H.button
-                ! A.class_ "accordion-button collapsed"
-                ! dataBsToggle "collapse"
-                ! (dataBsTarget . fromString $ "#" <> targetId)
-                ! A.type_ "button"
-                $ toHtml suite
-            H.div
-              ! A.class_ "accordion-collapse collapse"
-              ! (A.id . fromString $ targetId)
-              ! (dataBsParent . fromString $ "#" <> parentId)
-              $ H.div ! A.class_ "accordion-body" $
-                foldMap ((H.div ! A.class_ "accordion-item") . branchLink) branchRuns
+branchesHtml :: Int -> String -> [(String, [Html])] -> Html
+branchesHtml pos branch branchRunsHtml = do
+  let parentId = "branch-" <> branch
+  H.h2 ! A.class_ "m-2 mt-4 p-1" $ toHtml branch
+  H.div
+    ! A.class_ "accordion"
+    ! (A.id . fromString $ parentId) $
+    H.div ! A.class_ "accordion-item" $
+      (flip foldMap) branchRunsHtml $ \(suite, branchSuiteRuns) -> do
+        let targetId = "collapse-" <> branch <> "-" <> suite
+        H.h3 ! A.class_ "accordion-header" $
+          H.button
+            ! (A.class_ $
+                "accordion-button" <> (if pos == 0 then "" else " collapsed"))
+            ! dataBsToggle "collapse"
+            ! (dataBsTarget . fromString $ "#" <> targetId)
+            ! A.type_ "button"
+            $ toHtml suite
+        H.div
+          ! (A.class_ $
+              "accordion-collapse collapse" <> (if pos == 0 then " show" else ""))
+          ! (A.id . fromString $ targetId)
+          ! (dataBsParent . fromString $ "#" <> parentId) $
+          H.div ! A.class_ "accordion-body" $
+            H.div ! A.class_ "accordion-item" $ branchLink branchSuiteRuns
 
 branchesPage :: [Html] -> Html
 branchesPage branches = do
@@ -164,6 +194,7 @@ branchesPage branches = do
         H.h2 ! A.class_ "m-2 p-1" $ "automation-service - Branch Test Runs"
         fold branches
 
+
 --
 -- this is hacky test page generation code and I'm not being very
 -- careful about configuration, so keep in mind how many assumptions
@@ -174,8 +205,16 @@ generateSite :: IO ()
 generateSite = do
   setCurrentDirectory "ghp"
   files <- listDirectory "allure-action"
-  page <- branchesPage <$> branchesHtml files
-  writeFile "index.html" (renderMarkup page)
+  branchesRuns :: [(Integer, String, [(String, [Html])])] <- branchesRunsHtml files
+  let
+    branchesRuns' = sortBy (\(ts1, _b1, _rs1) (ts2, _b2, _rs2) -> compare ts2 ts1) branchesRuns
+    (_idx, branchesRunsOutput) =
+      foldl'
+        (\(idx, html) (_ts, branch, branchRuns) ->
+           (succ idx, html <> [branchesHtml idx branch branchRuns]))
+        (0, [])
+        branchesRuns'
+  writeFile "index.html" (renderMarkup $ branchesPage branchesRunsOutput)
 
 serve :: IO ()
 serve = run 8000 $ staticApp (defaultFileServerSettings ".")
