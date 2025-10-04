6ae7f8eabd32a3eb8cf8451a94a34eabc6505cd7
Dave Della Costa <ddellacosta@gmail.com>
2025-10-04 01:04:41 -0400

    Adds flag to toggle xml output vs. console for local dev

--- test/Main.hs
+++ test/Main.hs
@@ -5,23 +5,70 @@ module Main
   )
 where
 
+import Data.Functor ((<&>))
+import Data.List (isPrefixOf)
+import Data.Proxy (Proxy(..))
+import Data.Typeable (Typeable)
+import qualified Options.Applicative as OptApp
+import System.Environment (getArgs)
 import qualified Test.Integration.Service.Daemon as Daemon
-import Test.Tasty (TestTree, defaultIngredients, defaultMainWithIngredients, localOption, mkTimeout, testGroup)
+import Test.Tasty (TestTree, defaultIngredients, defaultMainWithIngredients, includingOptions, localOption, mkTimeout, testGroup)
 import Test.Tasty.Hspec (TreatPendingAs (..), testSpec)
-import qualified Test.Tasty.Runners.Reporter as Reporter
+import Test.Tasty.Ingredients.ConsoleReporter (consoleTestReporter)
+import Test.Tasty.Options (OptionDescription(..), IsOption(..))
+import qualified Test.Tasty.Runners.Reporter as CIReporter
 import qualified Test.Unit.Service.Device as Devices
 import qualified Test.Unit.Service.Group as Groups
 import qualified Test.Unit.Service.MQTT.Messages.Daemon as Daemon.Messages
 import qualified Test.Unit.Service.MQTT.Status as MQTTStatus
 import qualified Test.Unit.Service.TimeHelpers as TimeHelpers
 
+
 timeout :: Integer
 timeout = 10
 
+newtype ReporterOpt = ReporterOpt String
+  deriving (Eq, Ord, Typeable, Show)
+
+instance IsOption ReporterOpt where
+  defaultValue = ReporterOpt "ci"
+  parseValue = \case
+    "local" -> Just (ReporterOpt "local")
+    _ -> Just defaultValue
+  optionName = pure "reporter"
+  optionHelp =
+    pure "Reporter type: local or CI. Default is CI, which produces XML output for feeding into Allure Report. The 'local' option enables the default Tasty consoleTestReporter which is nicer to read when you're writing tests in your local environment."
+
+reporterParser :: OptApp.Parser ReporterOpt
+reporterParser = optionCLParser
+
+parseReporter :: [String] -> IO ReporterOpt
+parseReporter = OptApp.handleParseResult .
+  (OptApp.execParserPure
+    OptApp.defaultPrefs
+    (OptApp.info reporterParser OptApp.fullDesc))
+
 main :: IO ()
-main = defaultMainWithIngredients defaultIngredients' =<< allTests
+main = do
+  reporterArgs <- getArgs <&>
+    filter (\arg -> "--reporter" `isPrefixOf` arg)
+
+  reporter :: ReporterOpt <-
+    if null reporterArgs then
+      pure defaultValue
+    else
+      parseReporter reporterArgs
+
+  case reporter of
+    ReporterOpt "local" ->
+      defaultMainWithIngredients defaultIngredientsLocal' =<< allTests
+    _ ->
+      defaultMainWithIngredients defaultIngredientsCI' =<< allTests
+
   where
-    defaultIngredients' = Reporter.ingredient : defaultIngredients
+    reporterOptIngredient = includingOptions [Option (Proxy :: Proxy ReporterOpt)]
+    defaultIngredientsCI' = CIReporter.ingredient : reporterOptIngredient : defaultIngredients
+    defaultIngredientsLocal' = consoleTestReporter : reporterOptIngredient : defaultIngredients
 
 allTests :: IO TestTree
 allTests = do
