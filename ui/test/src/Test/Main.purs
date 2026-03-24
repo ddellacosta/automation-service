581cf3d2b5da85973e534f98572cf79ec8ff78a7
Dave Della Costa <ddellacosta@gmail.com>
2026-03-23 22:55:01 -0400

    Swaps mocha-headless-chrome and elmish-testing-library for a very basic playwright wrapper

--- ui/test/src/Test/Main.purs
+++ ui/test/src/Test/Main.purs
@@ -1,170 +1,128 @@
 module Test.Main where
 
-import AutomationService.Message (Message(..))
-import AutomationService.WebSocket (class WebSocket)
-import Data.Argonaut.Core (stringify)
-import Data.Map as M
+import Control.Monad.Error.Class (throwError)
 import Data.Maybe (Maybe(..))
-import Data.Time.Duration (Milliseconds(..))
-import Data.Traversable (for_)
-import Effect (Effect)
-import Effect.Aff (Aff, delay)
-import Effect.Aff.Class (liftAff)
-import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
+import Effect.Exception (error)
 import Effect.Ref (Ref)
 import Effect.Ref as Ref
-import Elmish.Component (Command)
--- see Test.AutomationService.Elmish.Bootstrap
--- import Elmish.Test (find, prop, testComponent, text, (>>))
--- see note about text replacement below
--- import Elmish.Test (find, prop, text, (>>))
-import Elmish.Test (find, prop, (>>))
-import Elmish.Test.DomProps as P
-import Elmish.Test.Events (change, click)
-import Foreign (unsafeFromForeign)
-import Main as Main
-import Prelude (Unit, bind, discard, pure, void, ($), (<<<), (>>>), (<$>), (<>), (>>=))
-import Test.AutomationService.Elmish.Bootstrap (testComponent)
-import Test.AutomationService.Spec (Spec)
-import Test.AutomationService.WebSocketStub (webSocketStub)
+import Prelude (($), (<>), (=<<), Unit, bind, discard, flip, pure)
 import Test.Fixtures as Fixtures
-import Test.Spec (before, describe, it, pending')
+import Test.Playwright as PW
+import Test.Playwright.RouteWebSocket as WSRoute
+import Test.Spec (Spec, after, before, describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Web.DOM.Element (Element, toNode)
-import Web.DOM.Node (textContent)
-import Web.Event.CustomEvent as CE
-import Web.Event.Event (EventType(..))
-import Web.Event.EventTarget (EventTarget, eventListener)
-import Web.Event.EventTarget as ET
 
---
--- For reasons that I don't understand, only when attempting to test
--- this in the context of a nix build does innerText seem to fail
--- here:
--- https://github.com/collegevine/purescript-elmish-testing-library/blob/f867a4c57f2f98fc731aa80506ecae22e57e78e2/src/Elmish/Test/Query.js#L1
---
--- As a result I just made my own stupid text function that doesn't
--- depend on (Elmist.Test.State.)Testable, and which I can use with
--- >>= instead of >>. Everything else seems fine ¯\_(ツ)_/¯
---
--- I'm sure there's a deeper reason this is happening that I should
--- probably care more about but I've spent hours yak-shaving this and
--- I'm kinda done with it. As long as I only have to modify test code
--- I don't care that much
---
-text :: ∀ m. MonadEffect m => Element -> m String
-text = toNode >>> textContent >>> liftEffect
 
-newtype TestWS = TestWS
-  { store :: Ref String
-  , ws :: EventTarget
+type TestContext =
+  { browser :: PW.Browser
+  , page :: PW.Page
+  , wsRoute :: Ref (Maybe WSRoute.WebSocketRoute)
+  , sentMessages :: Ref (Array String)
   }
 
-instance WebSocket TestWS where
-  sendJson (TestWS { store: wsStr }) s = Ref.write (stringify s) wsStr
-  sendString (TestWS { store: wsStr }) s = Ref.write s wsStr
-
-  addWSEventListener (TestWS { ws }) messageHandler = do
-    el <- liftEffect $ eventListener $ \evt -> do
-      for_ (CE.fromEvent evt) \customEvt -> do
-        let jsonStr = unsafeFromForeign $ CE.detail customEvt
-        messageHandler jsonStr
-    ET.addEventListener (EventType "fakeWebSocketMessage") el false ws
-
-connectToWS :: TestWS -> Command Aff (Message TestWS)
-connectToWS wsState { dispatch: msgSink } =
-  liftEffect <<< msgSink <<< InitWS "" $ wsState
-
-sendMessage :: EventTarget -> String -> Effect Unit
-sendMessage ws msg = do
-  let newCustomEvent = CE.new' (EventType "fakeWebSocketMessage") (Just msg)
-  msgEvt <- CE.toEvent <$> newCustomEvent
-  void $ ET.dispatchEvent msgEvt ws
-
 spec :: Spec Unit
-spec = before setup $
-  describe "Main app" $ do
+spec = before setup $ after teardown $
+  describe "Main app" $
+    it "Can navigate to different pages" $ \ctx@{ page } -> do
 
-    it "Can navigate to different pages" $ \wsState@(TestWS { store: _store, ws }) -> do
+      -- Wait for the app's WebSocket connection to be intercepted,
+      -- then send it device data as the server
+      wsRef <- liftEffect $ Ref.read ctx.wsRoute
+      case wsRef of
+        Nothing -> throwError (error "WS route not captured")
+        Just ws -> liftEffect $ do
+          WSRoute.sendToPage ws Fixtures.groupsWithBasementStandingLampFixture
+
+          WSRoute.sendToPage ws $
+            "[" <> Fixtures.coordinator <>
+            "," <> Fixtures.signeFixture <>
+            "," <> Fixtures.basementStandingLampBottomFixture <>
+            "," <> Fixtures.basementStandingLampTopFixture <>
+            "]"
+
+
+      -- Devices
+
+      PW.waitForSelector page "h2[data-test-id='main-title']"
+      title <- PW.textContent =<< PW.locator page "h2[data-test-id='main-title']"
+      title `shouldEqual` Just "Devices"
+
+      PW.click =<< PW.locator page "li[data-test-id='nav-devices'] a"
+      devTitle <- PW.textContent =<< PW.locator page "h2[data-test-id='main-title']"
+      devTitle `shouldEqual` Just "Devices"
+
+      device1Name <- PW.textContent =<< flip PW.nth 0 =<<
+        PW.locator page "div.all-devices div.device .card-body .card-header"
+      device1Name `shouldEqual` Just "Basement Black Signe"
+
+      device2Name <- PW.textContent =<< flip PW.nth 1 =<<
+        PW.locator page "div.all-devices div.device .card-body .card-header"
+      device2Name `shouldEqual` Just "Basement Standing Lamp Bottom"
+
+      device3Name <- PW.textContent =<< flip PW.nth 2 =<<
+        PW.locator page "div.all-devices div.device .card-body .card-header"
+      device3Name `shouldEqual` Just "Basement Standing Lamp Top"
+
+
+      -- Groups
+
+      -- this will fail if default_binding_group is present
+      groupName <- PW.textContent =<<
+        PW.locator page "div.all-devices div.group .card-body .card-header"
+      groupName `shouldEqual` Just "Basement Standing Lamp"
+
+      PW.pause page
+
+
+      -- Publish MQTT
+
+      PW.click =<< PW.locator page "li[data-test-id='nav-publish-mqtt'] a"
+
       let mqttMsg = "{\"start\": \"test\"}"
+      mqttInput <- PW.locator page "input[data-test-id='publish-mqtt-input']"
 
-      newDsUpdateTimers <- liftEffect $ Ref.new M.empty
+      PW.fill mqttInput mqttMsg
+      PW.click =<< PW.locator page "button[data-test-id='publish-mqtt-btn']"
 
-      testComponent
-        { init: Main.init newDsUpdateTimers $ connectToWS wsState
-        , view: Main.view
-        , update: Main.update
-        } do
+      lastSent <- PW.textContent =<< PW.locator page "div[data-test-id='last-sent-msg']"
+      lastSent `shouldEqual` Just ("Last sent:" <> mqttMsg)
 
-          --
-          -- I seem to need a bit of a delay to ensure init is done
-          -- running. Based on local timings, we'll see how this
-          -- works when in CI. So I don't care for this much
-          --
-          liftAff $ delay $ Milliseconds 250.0
+      -- Also verify what the page sent via the WS (the publish msg)
+      sent <- liftEffect $ Ref.read ctx.sentMessages
 
-          liftEffect $
-            sendMessage ws $ "[" <> Fixtures.signeFixture <> "]"
+      -- sent will contain the JSON the app sent via sendString/sendJson
+      sent `shouldEqual` [
+        "{\"topic\":\"zigbee2mqtt/Basement Black Signe\",\"subscribe\":\"HTTP 8850\"}","{\"topic\":\"zigbee2mqtt/Basement Black Signe/get\",\"publish\":{\"state\":\"\"}}","{\"start\": \"test\"}"
+        ]
 
-          find ("h2" `withTestId` "main-title") >>= text
-            >>= shouldEqual "Devices"
+      -- PW.pause page
 
-          -- Devices
-          find ("li" `withTestId` "nav-devices" <> " a") >> click
-          find ("h2" `withTestId` "main-title") >>= text
-            >>= shouldEqual "Devices"
+      teardown ctx
 
-          find "div.all-devices div.device .card-body .card-header" >>= text
-            >>= shouldEqual "Basement Black Signe"
+  where
+    setup :: Aff TestContext
+    setup = do
+      browser <- PW.launch { headless: false }
+      page <- PW.newPage browser
 
-          -- Publish MQTT
-          find ("li" `withTestId` "nav-publish-mqtt" <> " a") >> click
-          find ("h2" `withTestId` "main-title") >>= text
-            >>= shouldEqual "Publish MQTT"
+      -- Refs to capture the route and outgoing messages
+      wsRouteRef <- liftEffect $ Ref.new Nothing
+      sentRef <- liftEffect $ Ref.new []
 
-          find ("li" `withTestId` "nav-publish-mqtt" <> " a") >> click
-          find ("input" `withTestId` "publish-mqtt-input") >> change mqttMsg
+      -- Intercept ALL WebSocket connections the page makes.
+      WSRoute.routeWebSocket page "**" \ws -> do
+        Ref.write (Just ws) wsRouteRef
 
-          find ("input" `withTestId` "publish-mqtt-input") >> prop P.value
-             >>= shouldEqual mqttMsg
+        -- Capture messages the app sends to the server
+        WSRoute.onMessage ws \msg ->
+          Ref.modify_ (_ <> [msg]) sentRef
 
-          find ("button" `withTestId` "publish-mqtt-btn") >> click
+      -- Navigate to the app, make this configurable
+      PW.goto page "http://localhost:8850"
 
-          --
-          -- with this uncommented or the comparison with mqttMsg
-          -- below this blows up only in nix build for reasons I
-          -- don't understand, will look into it more closely soon
-          -- - 2025-02-09
-          --
-          -- liftAff $ delay $ Milliseconds 250.0
+      pure { browser, page, wsRoute: wsRouteRef, sentMessages: sentRef }
 
-          find ("div" `withTestId` "last-sent-msg") >>= text
-            >>= shouldEqual ("Last sent:" <> mqttMsg)
-
-          -- wsStr <- liftEffect $ Ref.read store
-          -- wsStr `shouldEqual` mqttMsg
-
-
-    -- it "Groups are loaded and operational" $ \wsState@(TestWS { store: _store, ws }) -> do
-    pending' "Groups are loaded and operational" do
---     it "Groups are loaded and operational" $ \wsState -> do
---
---       newDsUpdateTimers <- liftEffect $ Ref.new M.empty
---
---       testComponent
---         { init: Main.init newDsUpdateTimers $ connectToWS wsState
---         , view: Main.view
---         , update: Main.update
---         } do
-
-          (1 :: Int) `shouldEqual` (2 :: Int)
-
-   where
-     setup :: Aff TestWS
-     setup = do
-       store <- liftEffect $ Ref.new "foo"
-       ws <- liftEffect webSocketStub
-       pure $ TestWS { store, ws }
-
-     withTestId :: String -> String -> String
-     withTestId sel testId = sel <> "[data-test-id=\"" <> testId <> "\"]"
+    teardown :: TestContext -> Aff Unit
+    teardown ctx = PW.close ctx.browser
